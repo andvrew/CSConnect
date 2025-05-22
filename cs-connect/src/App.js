e1362edf8f3485114319fc87e81a228e38be7c7e
Andrew <seyiababs@gmail.com>
2025-05-22 11:33:15 -0400

    first attempt

---
+++ cs-connect/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="CSConnect">
+      <header className="CSConnect-header">
+        <p>
+          Connect Now!
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
